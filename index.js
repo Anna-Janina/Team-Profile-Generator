f0a18d9187d8e12c6e03f8823b7823a00bfd04e5
Anna <annajaninaschmidt@gmail.com>
2023-02-03 08:33:49 +1100

    folder set up

---
+++ index.js
@@ -0,0 +1,18 @@
+// Import npm packages
+const inquirer = require('inquirer');
+const fs = require('fs');
+const ??? = require('???');
+
+// Import classes
+
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+
+
+    ])
