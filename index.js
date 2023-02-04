499c422d179b955c96808b53e395ebaf6081ba75
Anna <annajaninaschmidt@gmail.com>
2023-02-04 20:26:46 +1100

    added engineer.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
 
-// const Employee = require('./lib/Employee');
-// const Engineer = require('./lib/Engineer');
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
 // const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
